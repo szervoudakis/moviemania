b5c5d8ce60a86599a4681e3edbab158a4cbdddd6
szervoudakis <stezer95@gmail.com>
2025-09-05 11:21:43 +0300

    use lazy loading to enable code splitting and for better performance

--- themes/movie_mania_theme/react_app/src/App.js
+++ themes/movie_mania_theme/react_app/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Top250Movies from './pages/top250Movies';
-import DashboardPage from './pages/DashboardPage';
+
+//lazy loading the page components to enable code splitting and improve performance
+const Home = lazy(() => import("./pages/Home"));
+const Top250Movies = lazy(() => import("./pages/top250Movies"));
+const DashboardPage = lazy(()=> import("./pages/DashboardPage"));
 
 function App() {
+  
   const topMoviesRoute = window.drupalSettings.movieMania.topMoviesRoute;
   const top3movies = window.drupalSettings.movieMania.top3movies;
   const api_url_movies = window.drupalSettings.movieMania.topmovies;
   const api_url_years = window.drupalSettings.movieMania.years;
   const dashboard = window.drupalSettings.movieMania.dashboardRoute;
   const user = window.drupalSettings.movieMania.user;
-
-  console.log(window.drupalSettings.movieMania);
+ 
   return (
     <Router>
-      <Routes>
-          <Route path="/" element={<Home top3movies={top3movies} />} />
-          <Route path={topMoviesRoute} element={<Top250Movies api_url_movies={api_url_movies} api_url_years={api_url_years} />} />
-          <Route path={dashboard} element={<DashboardPage user={user} />} />
-      </Routes>
+       <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+              <Route path="/" element={<Home top3movies={top3movies} />} />
+              <Route path={topMoviesRoute} element={<Top250Movies api_url_movies={api_url_movies} api_url_years={api_url_years} />} />
+              <Route path={dashboard} element={<DashboardPage user={user} />} />
+          </Routes>
+      </Suspense>
     </Router>
   );
 }
